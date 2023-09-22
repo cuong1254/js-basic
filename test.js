f910d276b3ade8b57976521b6e2c7b95828666be
unknown <cuong1254>
2023-09-22 19:31:04 +0700

    Bai1

---
+++ test.js
@@ -0,0 +1,30 @@
+//Bai 2:
+//a)
+//let a = 10
+//console.log(a)
+//let b = '10'
+//console.log(b)
+//a =20
+//console.log(a)
+//let pi = 3.14
+//console.log(pi)
+//pi = 3.14 *3
+//console.log(pi)
+//let hocSinh = {
+    //hoTen: 'Vu Duc Hung Cuong',
+    //tuoi: 17,
+    //diaChi: 'GoVap - Tphcm'
+//}
+//hocSinh.soDienThoai = 372003786
+//hocSinh.diaChi = 'Quan 1'
+//console.log('==>', hocSinh)
+
+let dienThoai = {
+    ten: 'iPhone 14',
+    hangSanXuat: 'Apple',
+    mauSac: 'Trang',
+    gia: '1000USD'
+}
+dienThoai.gia = '750USD'
+dienThoai.hangSanXuat = 'iPhone 14 promax'
+console.log('==>', dienThoai)
