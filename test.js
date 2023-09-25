ca3cb193f6197dab795e8096096f4e7e6dbd2459
unknown <cuong1254>
2023-09-25 19:29:56 +0700

    Bai2

--- test.js
+++ test.js
@@ -19,12 +19,56 @@
 //hocSinh.diaChi = 'Quan 1'
 //console.log('==>', hocSinh)
 
-let dienThoai = {
-    ten: 'iPhone 14',
-    hangSanXuat: 'Apple',
-    mauSac: 'Trang',
-    gia: '1000USD'
-}
-dienThoai.gia = '750USD'
-dienThoai.hangSanXuat = 'iPhone 14 promax'
-console.log('==>', dienThoai)
+//let dienThoai = {
+  //  ten: 'iPhone 14',
+    //hangSanXuat: 'Apple',
+    //mauSac: 'Trang',
+    //gia: '1000USD'
+//}
+//dienThoai.gia = '750USD'
+//dienThoai.hangSanXuat = 'iPhone 14 promax'
+//console.log('==>', dienThoai)
+
+let a = 5 , b = 7
+let kq = a + b
+console.log(kq)
+let c = 5 , d = 7
+tbc = (c+d) / 2
+console.log(tbc)
+console.log('=============================')
+let x = 3, y = 4, z = 6
+console.log('x^2 + 2x + 1 =', x**2 + 2*x + 1)
+console.log('x^3 - 3xy - 5y + 3y^2 =', x**3 - 3*x*y - 5*y + 3*y**2)
+console.log('(xy)^2 - 2x^2*y + 13y =', (x*y)**2 - 2*x**2*y + 13*y)
+console.log('4x^3 + 3xy + y^2 - (2x^2 - 3y) =', 4*x*x*x + 3*x*y + y*y - 2*x*x + 3*y)
+console.log('(5x^2 / 4xy) + y^2 =', (5*x*x / 4*x*y) + y*y)
+console.log('x^2 - 2zxy / y^2 + 5x - 2y^2 + 4xz^3 + z^3 =', x*x - 2*x*y*z / y*y + 5*x - 2*y*y + 4*x*z*z*z + z*z*z)
+console.log('=============================')
+let dk = 5
+console.log('Chu vi hinh tron la: ', dk*3.14)
+console.log('Dien tich hinh tron la: ', (dk/2)**2*3.14)
+console.log('=============================')
+let cd = 15, cr = 10
+console.log('Chieu dai hcn: ', cd)
+console.log('Chieu rong hcn: ', cr)
+console.log('Chu vi hcn la =', (cd+cr)/2)
+console.log('Dien tich hcn la =', cd*cr)
+console.log('=============================')
+let M = 120 //km
+let T = 130/60 //gio
+console.log('Toc do cua xe la', M / T, 'km/h')
+console.log('=============================')
+let s = 15.2 * 3
+console.log('Quang duong cano di dc =', s)
+console.log('=============================')
+let tg = 3 + 20/60
+console.log('Thoi gian o to di dc la: 11 giờ - 8 giờ 20 phút =', 3,'h 20phut =', tg,'h')
+console.log('Do dai quang duong AB =', 42 * tg)
+console.log('=============================')
+let khoi_duoi = ( 8 + 8 + 8 )*5*6
+let khoi_tren = 8*6*5
+console.log('The tich cua khoi go la =', khoi_duoi + khoi_tren)
+
+
+
+
