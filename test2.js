6066bda3b64d54e15ecc3f90dc11705169ad6ade
unknown <cuong1254>
2023-09-25 19:41:15 +0700

    Bai1

---
+++ test2.js
@@ -0,0 +1,13 @@
+function tinhTong(a,b) {
+    const kq = a+b
+    return kq
+}
+const kq1 = tinhTong(10, 11)
+console.log(kq1)
+
+function tinhHieu(c,d) {
+    const hieu = c - d
+    return hieu
+}
+const hieu1 = tinhHieu(10, 11)
+console.log(hieu1)
