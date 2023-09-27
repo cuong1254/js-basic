038ef34baf7b5bd69df4789f9eeafae86b06aacd
unknown <cuong1254>
2023-09-27 18:27:07 +0700

    If_else

---
+++ test3.js
@@ -0,0 +1,12 @@
+//Bai 1: a)
+function kienTra_Am_Duong(n) {
+    if (n>0) {
+        console.log('So n am')
+    }
+    else if (n>0) {
+        console.log('So n duong')
+    }
+    else {
+        console.log('So n la so 0')
+    }
+}
