8f8cdf9882047a07a18b27cd47267cb94b9f5606
unknown <cuong1254>
2023-09-27 18:13:06 +0700

    IF_ELSE

--- test2.js
+++ test2.js
@@ -11,3 +11,16 @@ function tinhHieu(c,d) {
 }
 const hieu1 = tinhHieu(10, 11)
 console.log(hieu1)
+
+function tinhCV(cd,cr) {
+    const cv = ( cd + cr) *2
+    return cv
+}
+const dap_an1 = tinhCV(1, 2)
+console.log('CV =', dap_an1)
+function tinhDT(cd,cr) {
+    const dt = cd * cr
+    return dt
+}
+const dap_an2 = tinhCV(1, 2)
+console.log('DT =', dap_an2)
