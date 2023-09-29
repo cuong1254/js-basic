61d18f9528511219d419a4803e61ff19ee0622c4
unknown <cuong1254>
2023-09-29 19:43:14 +0700

    Loop

--- test4.js
+++ test4.js
@@ -1,1 +1,8 @@
-console.log('HELLO!!!')
+//Bai 1:
+let arr = [3, 451, 5, 2, 6, 76, 3, 5, 1, 243, 6, 4]
+//Vong lap for:
+let doDaiMang = arr.length
+for (let i = 0; i < doDaiMang; i = i + 1){
+    console.log('Gia tri mang %s = %s', i, arr[i])
+}
+//Vong lap while:
